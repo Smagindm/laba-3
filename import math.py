ca51d459a5d685092a20f500938b924f11c45aae
Smagindm <dimasmg62@gmail.com>
2026-09-25 11:07:57 +0000

    2 zadanie

--- import math.py
+++ import math.py
@@ -1,6 +1,8 @@
 import math
-x=0.75
-numerator = math.asin(x)**2 + math.acos(x)**2
-demirator = math.sin(x)**2+math.cos(x)**2
-y=(2**x)*numerator/demirator
-print(f"Значение при x= {x}:{y}")
+a=-0.2
+b=0.9
+x= (a+b) / 2
+term1 = (x*math.sin(x)**2)/math.cos(2*x)
+term2 = (math.exp(-2*x)*(math.cos(x)+math.sin(x))) / (math.sin(x) - math.cos(x))
+y = x**2 * (term1 + term2)
+print(f"Значение при x = {x}:{y}")
